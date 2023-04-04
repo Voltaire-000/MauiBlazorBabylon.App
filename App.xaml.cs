9ce7e8d54ba9aa64c8966768452c77abe2b62903
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2023-04-04 19:23:00 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorBabylon;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorBabylon;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
